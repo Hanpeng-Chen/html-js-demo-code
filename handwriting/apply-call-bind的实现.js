684dd314e6ad3a8b33879ce0dae9678091b8420b
Hanpeng Chen <hanpengchen07@163.com>
2021-03-09 17:26:57 +0800

    handwriting new and apply code

---
+++ handwriting/apply-call-bind的实现.js
@@ -0,0 +1,15 @@
+Function.prototype.apply = function(context, args) {
+  let context = context || window;
+  context.fn = this;
+  let result = eval('context.fn(...args)')
+  delete context.fn
+  return result
+}
+
+Function.prototype.call = function(context, ...args) {
+  let context = context || window;
+  context.fn = this;
+  let result = eval('context.fn(...args)')
+  delete context.fn
+  return result
+}
